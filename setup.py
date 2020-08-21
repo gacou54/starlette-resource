40949169f35a44679a679478137632692292ce26
Gabriel Couture <gacou54@gmail.com>
2020-08-21 14:21:05 -0400

    Add README in the setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+from os import path
+
 from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='starlette-resource',
@@ -10,5 +17,7 @@ setup(
     author_email='gacou54@gmail.com',
     description='Starlette resource that helps you follow a layered architecture.',
     package=['starlette_resource'],
-    install_requires=['starlette']
+    install_requires=['starlette'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
